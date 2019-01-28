cee271d4a70a1597c52f9b3613fdc04b18871352
jppineda152808@gmail.com <jppineda152808@gmail.com>
2019-01-28 19:36:02 +0800

    fixed issue

--- app/src/main/java/go/and/fast/com/fastandgo/mainMenu/MainMenu.java
+++ app/src/main/java/go/and/fast/com/fastandgo/mainMenu/MainMenu.java
@@ -1,18 +1,14 @@
 package go.and.fast.com.fastandgo.mainMenu;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 
 import go.and.fast.com.fastandgo.R;
 import go.and.fast.com.fastandgo.mainMenu.foodMenu.FoodScreen;
